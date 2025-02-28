200715439f74b2c2a150f54c50b9dfe5b50afe97
Sebastian Barros <sebastian.barros02@usa.edu.co>
2025-02-28 14:37:40 -0500

    add: folder schema

--- app/database.py
+++ app/database.py
@@ -1,33 +1,33 @@
-from sqlmodel import Session, create_engine, SQLModel
-from typing import Annotated
-from fastapi import Depends, FastAPI
+# from sqlmodel import Session, create_engine, SQLModel
+# from typing import Annotated
+# from fastapi import Depends, FastAPI
 
-sqlite_name = "db.sqlite3"
-sqlite_url = f"sqlite:///./{sqlite_name}"
+# sqlite_name = "db.sqlite3"
+# sqlite_url = f"sqlite:///./{sqlite_name}"
 
 
-engine = create_engine(sqlite_url)
+# engine = create_engine(sqlite_url)
 
-def create_all_tables(app: FastAPI):
-    SQLModel.metadata.create_all(engine) 
-    yield
+# def create_all_tables(app: FastAPI):
+#     SQLModel.metadata.create_all(engine) 
+#     yield
 
-def get_session():
-    with Session(engine) as session:
-        yield session
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
         
-SessionDep = Annotated[Session, Depends(get_session)]
+# SessionDep = Annotated[Session, Depends(get_session)]
 
-# # database.py
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker, declarative_base
+# # # database.py
+# # from sqlalchemy import create_engine
+# # from sqlalchemy.orm import sessionmaker, declarative_base
 
-# # URL de conexión para SQLite local (archivo "test.db" en la carpeta actual)
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+# # # URL de conexión para SQLite local (archivo "test.db" en la carpeta actual)
+# # SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL,
-#     connect_args={"check_same_thread": False}  # Solo necesario para SQLite
-# )
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
+# # engine = create_engine(
+# #     SQLALCHEMY_DATABASE_URL,
+# #     connect_args={"check_same_thread": False}  # Solo necesario para SQLite
+# # )
+# # SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# # Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -1,110 +1,110 @@
-from fastapi import FastAPI, Depends, HTTPException
-from typing import List, Dict
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse, RedirectResponse
-# from sqlalchemy.orm import Session
-from sqlmodel import select
-from .models import PsycologistCreate, Psycologist, PsycologistLogin
-from .database import SessionDep, create_all_tables
-from .security import hash_password, verify_password
-from fastapi.middleware.cors import CORSMiddleware
-# from . import models, schemas
-# from .schemas import PsicologoCreate, PsicologoLogin, PsicologoOut
+# from fastapi import FastAPI, Depends, HTTPException
+# from typing import List, Dict
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import HTMLResponse, RedirectResponse
+# # from sqlalchemy.orm import Session
+# from sqlmodel import select
+# from .models import PsycologistCreate, Psycologist, PsycologistLogin
+# from .database import SessionDep, create_all_tables
+# from .security import hash_password, verify_password
+# from fastapi.middleware.cors import CORSMiddleware
+# # from . import models, schemas
+# # from .schemas import PsicologoCreate, PsicologoLogin, PsicologoOut
 
-# # Crear tablas en la base de datos
-# models.Base.metadata.create_all(bind=engine)
+# # # Crear tablas en la base de datos
+# # models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(lifespan= create_all_tables)
+# app = FastAPI(lifespan= create_all_tables)
 
-# Allow all origins (for development) - Restrict in production!
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Change to your frontend URL in production (e.g., ["http://localhost:3000"])
-    allow_credentials=True,
-    allow_methods=["*"],  # Allows GET, POST, OPTIONS, etc.
-    allow_headers=["*"],  # Allows all headers
-)
+# # Allow all origins (for development) - Restrict in production!
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # Change to your frontend URL in production (e.g., ["http://localhost:3000"])
+#     allow_credentials=True,
+#     allow_methods=["*"],  # Allows GET, POST, OPTIONS, etc.
+#     allow_headers=["*"],  # Allows all headers
+# )
 
-# Montar carpeta 'static' para servir archivos estáticos
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
-
-@app.get("/", response_class=RedirectResponse)
-def root():
-    return RedirectResponse("/login")
+# # Montar carpeta 'static' para servir archivos estáticos
+# app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
 
-# Ruta para servir index.html
-@app.get("/login", response_class=HTMLResponse)
-async def get_index():  
-    with open("app/static/index.html", "r") as f:
-        return HTMLResponse(content=f.read())
+# @app.get("/", response_class=RedirectResponse)
+# def root():
+#     return RedirectResponse("/login")
+
+
+# # Ruta para servir index.html
+# @app.get("/login", response_class=HTMLResponse)
+# async def get_index():  
+#     with open("app/static/index.html", "r") as f:
+#         return HTMLResponse(content=f.read())
     
-# # Dependencia para obtener una sesión de base de datos en cada request
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+# # # Dependencia para obtener una sesión de base de datos en cada request
+# # def get_db():
+# #     db = SessionLocal()
+# #     try:
+# #         yield db
+# #     finally:
+# #         db.close()
 
 
-# 📌 Endpoint de registro
-@app.post("/register_psycologist", response_model=Psycologist)
-def create_psicologist(pyscologist_data: PsycologistCreate, session: SessionDep):
+# # 📌 Endpoint de registro
+# @app.post("/register_psycologist", response_model=Psycologist)
+# def create_psicologist(pyscologist_data: PsycologistCreate, session: SessionDep):
 
-    new_psycologyst = Psycologist.model_validate(pyscologist_data.model_dump())
-    new_psycologyst.password = hash_password(new_psycologyst.password)
-    session.add(new_psycologyst)
-    session.commit()
+#     new_psycologyst = Psycologist.model_validate(pyscologist_data.model_dump())
+#     new_psycologyst.password = hash_password(new_psycologyst.password)
+#     session.add(new_psycologyst)
+#     session.commit()
 
-    # db.refresh(nuevo_psicologo)
-    # Verificar si el email ya está registrado
-    # db_psicologo = db.query(Psicologo).filter(Psicologo.email == pyscologist_data.email).first()
-    # if db_psicologo:
-    #     raise HTTPException(status_code=400, detail= 'El correo ya existe')
+#     # db.refresh(nuevo_psicologo)
+#     # Verificar si el email ya está registrado
+#     # db_psicologo = db.query(Psicologo).filter(Psicologo.email == pyscologist_data.email).first()
+#     # if db_psicologo:
+#     #     raise HTTPException(status_code=400, detail= 'El correo ya existe')
 
 
-    # # Crear nuevo psicólogo
-    # nuevo_psicologo = Psicologo(
-    #     email = pyscologist_data.email,
-    #     name = pyscologist_data.name,        
-    #     password=pyscologist_data.password,
-    #     age=pyscologist_data.age
-    # )
-    # db.add(nuevo_psicologo)
-    # db.commit()
-    # db.refresh(nuevo_psicologo)
-    return new_psycologyst
+#     # # Crear nuevo psicólogo
+#     # nuevo_psicologo = Psicologo(
+#     #     email = pyscologist_data.email,
+#     #     name = pyscologist_data.name,        
+#     #     password=pyscologist_data.password,
+#     #     age=pyscologist_data.age
+#     # )
+#     # db.add(nuevo_psicologo)
+#     # db.commit()
+#     # db.refresh(nuevo_psicologo)
+#     return new_psycologyst
 
              
     
 
-# 📌 Endpoint de login
-# @app.post("/login")
-# def login(login_data: schemas.PsicologoLogin, db: Session = Depends(get_db)):
-#     # Buscar al psicólogo por email
-#     psicologo = db.query(Psicologo).filter(Psicologo.email == login_data.email).first()
-#     if not psicologo: 
-#         raise HTTPException(status_code=401, detail="Credenciales inválidas")
+# # 📌 Endpoint de login
+# # @app.post("/login")
+# # def login(login_data: schemas.PsicologoLogin, db: Session = Depends(get_db)):
+# #     # Buscar al psicólogo por email
+# #     psicologo = db.query(Psicologo).filter(Psicologo.email == login_data.email).first()
+# #     if not psicologo: 
+# #         raise HTTPException(status_code=401, detail="Credenciales inválidas")
     
-    # # Verificar contraseña (en este ejemplo no está hasheada, ¡ojo en producción!)
-    # if psicologo.password != login_data.password:
-    #     raise HTTPException(status_code=401, detail="Invalid credentials")
+#     # # Verificar contraseña (en este ejemplo no está hasheada, ¡ojo en producción!)
+#     # if psicologo.password != login_data.password:
+#     #     raise HTTPException(status_code=401, detail="Invalid credentials")
     
-    # return {"message": f"Welcome {psicologo.name}"}
+#     # return {"message": f"Welcome {psicologo.name}"}
 
-@app.post("/login_psycologist")
-def login_psycologist(login_data: PsycologistLogin, session: SessionDep):
-    # 1️⃣ Buscar al psicólogo por email
-    psycologist = session.exec(select(Psycologist).where(Psycologist.email == login_data.email)).first()
+# @app.post("/login_psycologist")
+# def login_psycologist(login_data: PsycologistLogin, session: SessionDep):
+#     # 1️⃣ Buscar al psicólogo por email
+#     psycologist = session.exec(select(Psycologist).where(Psycologist.email == login_data.email)).first()
     
-    if not psycologist or not verify_password(login_data.password, psycologist.password):
-        raise HTTPException(status_code=401, detail="Invalid email or password")
+#     if not psycologist or not verify_password(login_data.password, psycologist.password):
+#         raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    return {"message": f"Welcome {psycologist.name}", "id": psycologist.id}
+#     return {"message": f"Welcome {psycologist.name}", "id": psycologist.id}
 
 
-@app.get("/psycologist", response_model=list[Psycologist])
-def list_psicologists(session:SessionDep):
-     return session.exec(select(Psycologist)).all()
+# @app.get("/psycologist", response_model=list[Psycologist])
+# def list_psicologists(session:SessionDep):
+#      return session.exec(select(Psycologist)).all()
